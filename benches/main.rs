2f81e0045201a9c1d1b74a9230d25348a3ddee31
dignifiedquire <me@dignifiedquire.com>
2019-11-13 11:47:56 +0100

    improve repo layout and docs

--- benches/main.rs
+++ benches/main.rs
@@ -6,35 +6,69 @@ use test::Bencher;
 
 use byte_pool::BytePool;
 
-#[bench]
-fn base_line_vec_4k(b: &mut Bencher) {
-    let size = 4 * 1024;
+fn touch_obj(buf: &mut [u8], size: usize) {
+    assert_eq!(buf.len(), size);
+    buf[0] = 1;
+}
 
-    b.bytes = size as u64;
+macro_rules! benches_for_size {
+    ($size:expr, $name1:ident, $name2:ident) => {
+        #[bench]
+        fn $name1(b: &mut Bencher) {
+            b.bytes = $size as u64;
+
+            b.iter(|| {
+                // alloc
+                let mut buf = vec![0u8; $size];
+                touch_obj(&mut buf, $size);
+            });
+        }
+
+        #[bench]
+        fn $name2(b: &mut Bencher) {
+            b.bytes = $size as u64;
+            let pool = BytePool::new();
+
+            b.iter(|| {
+                // alloc
+                let mut buf = pool.alloc($size);
+                touch_obj(&mut buf, $size);
+            });
+        }
+    };
+}
+
+benches_for_size!(256, base_line_vec_256b, pool_256b);
+benches_for_size!(1 * 1024, base_line_vec_1k, pool_1k);
+benches_for_size!(4 * 1024, base_line_vec_4k, pool_4k);
+benches_for_size!(8 * 1024, base_line_vec_8k, pool_8k);
+
+#[bench]
+fn base_line_vec_mixed(b: &mut Bencher) {
+    let mut i = 0;
 
     b.iter(|| {
-        // alloc
+        // alternate between two sizes
+        let size = if i % 2 == 0 { 1024 } else { 4096 };
         let mut buf = vec![0u8; size];
-        // write manual, simulating a Write::write call
-        for i in 0..size {
-            buf[i] = 1;
-        }
+        touch_obj(&mut buf, size);
+
+        i += 1;
     });
 }
 
 #[bench]
-fn byte_pool_4k(b: &mut Bencher) {
-    let size = 4 * 1024;
+fn pool_mixed(b: &mut Bencher) {
+    let mut i = 0;
 
-    b.bytes = size as u64;
     let pool = BytePool::new();
 
     b.iter(|| {
-        // alloc
+        // alternate between two sizes
+        let size = if i % 2 == 0 { 1024 } else { 4096 };
         let mut buf = pool.alloc(size);
-        // write manual, simulating a Write::write call
-        for i in 0..size {
-            buf[i] = 1;
-        }
+        touch_obj(&mut buf, size);
+
+        i += 1;
     });
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,150 @@
-use std::ops::{Deref, DerefMut};
+//! Pool of byte slices.
+//!
+//! # Example
+//! ```rust
+//! use byte_pool::BytePool;
+//!
+//! // Create a pool
+//! let pool = BytePool::new();
+//!
+//! // Allocate a buffer
+//! let mut buf = pool.alloc(1024);
+//!
+//! // write some data into it
+//! for i in 0..100 {
+//!   buf[i] = 12;
+//! }
+//!
+//! // Check that we actually wrote sth.
+//! assert_eq!(buf[55], 12);
+//!
+//! // Returns the underlying memory to the pool.
+//! drop(buf);
+//!
+//! // Frees all memory in the pool.
+//! drop(pool);
+//! ```
 
+use std::alloc::{alloc, dealloc, handle_alloc_error, Layout};
+use std::mem;
+use std::ops::{Deref, DerefMut};
+use std::ptr::{self, NonNull};
+use std::sync::Mutex;
+
+/// A pool of byte slices, that reuses memory.
 pub struct BytePool {
-    // list: Vec<Block>,
+    list: Mutex<Vec<RawBlock>>,
+}
+
+pub struct RawBlock {
+    ptr: NonNull<u8>,
+    layout: Layout,
 }
 
 pub struct Block<'a> {
+    data: mem::ManuallyDrop<RawBlock>,
     pool: &'a BytePool,
-    data: Box<[u8]>,
 }
 
 impl Default for BytePool {
     fn default() -> Self {
-        BytePool { /*list: Vec::new()*/ }
+        BytePool {
+            list: Mutex::new(Vec::new()),
+        }
     }
 }
 
+fn layout_for_size(size: usize) -> Layout {
+    let elem_size = mem::size_of::<u8>();
+    let alloc_size = size.checked_mul(elem_size).unwrap();
+    let align = mem::align_of::<u8>();
+    Layout::from_size_align(alloc_size, align).unwrap()
+}
+
 impl BytePool {
+    /// Constructs a new pool.
     pub fn new() -> Self {
         BytePool::default()
     }
 
+    /// Allocates a new `Block`, which represents a fixed sice byte slice.
+    /// If `Block` is dropped, the memory is _not_ freed, but rather it is returned into the pool.
     pub fn alloc(&self, size: usize) -> Block<'_> {
-        // TODO: reuse blocks
+        assert!(size > 0, "Can not allocate empty blocks");
 
-        Block::new(size, &self)
+        // check the last 4 blocks
+        let mut lock = self.list.lock().unwrap();
+        let end = lock.len();
+        let start = if end > 4 { end - 4 } else { 0 };
+
+        for i in start..end {
+            if lock[i].layout.size() == size {
+                // found one, reuse it
+                return Block::new(lock.remove(i), self);
+            }
+        }
+        drop(lock);
+
+        // allocate a new block
+        let data = RawBlock::alloc(size);
+        Block::new(data, self)
+    }
+
+    fn push_raw_block(&self, block: RawBlock) {
+        self.list.lock().unwrap().push(block);
+    }
+}
+
+impl<'a> Drop for Block<'a> {
+    fn drop(&mut self) {
+        let data = mem::ManuallyDrop::into_inner(unsafe { ptr::read(&self.data) });
+        self.pool.push_raw_block(data);
+    }
+}
+
+impl RawBlock {
+    pub fn alloc(size: usize) -> Self {
+        // TODO: consider caching the layout
+        let layout = layout_for_size(size);
+        debug_assert!(layout.size() > 0);
+
+        let ptr = unsafe { alloc(layout) };
+        RawBlock {
+            ptr: NonNull::new(ptr).unwrap_or_else(|| handle_alloc_error(layout)),
+            layout,
+        }
+    }
+}
+
+impl Drop for RawBlock {
+    fn drop(&mut self) {
+        unsafe {
+            dealloc(self.ptr.as_mut(), self.layout);
+        }
+    }
+}
+
+impl Deref for RawBlock {
+    type Target = [u8];
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        unsafe { std::slice::from_raw_parts(self.ptr.as_ptr(), self.layout.size()) }
+    }
+}
+
+impl DerefMut for RawBlock {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { std::slice::from_raw_parts_mut(self.ptr.as_mut(), self.layout.size()) }
     }
 }
 
 impl<'a> Block<'a> {
-    fn new(size: usize, pool: &'a BytePool) -> Self {
-        // TODO: use manual allocation
-
+    pub fn new(data: RawBlock, pool: &'a BytePool) -> Self {
         Block {
+            data: mem::ManuallyDrop::new(data),
             pool,
-            data: vec![0u8; size].into_boxed_slice(),
         }
     }
 }
@@ -43,14 +154,14 @@ impl<'a> Deref for Block<'a> {
 
     #[inline]
     fn deref(&self) -> &Self::Target {
-        &*self.data
+        self.data.deref()
     }
 }
 
 impl<'a> DerefMut for Block<'a> {
     #[inline]
     fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut *self.data
+        self.data.deref_mut()
     }
 }
 
